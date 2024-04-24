c3a3b7e1fe80a22723d81b13cb5192824cd157eb
luisduran98 <104513661+luisduran98@users.noreply.github.com>
2024-04-23 23:26:05 -0300

    actualizacion

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Luis 
+apellido: Duran
 ---
 Ejercicio: entrada_salida_02
 ---
@@ -27,7 +27,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        mensaje = prompt("Mensaje","Escribe lo que quieras: ")
+        alert("Alerta",mensaje)
         
         
     
